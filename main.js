684039bb8f96352e03720869087e7d08cbfdf975
Markus Mlynarski <3245338+mightymakku@users.noreply.github.com>
2019-03-30 21:19:59 +0100

    Add Promise.all() and Promise.race().

--- main.js
+++ main.js
@@ -22,15 +22,54 @@
         });
     }
 
-    function promiseFunction() {
+    const multiButton = document.getElementById("multi-button");
+    const multiStatus = document.getElementById("multi-status");
+    const multiTypeSelect = document.getElementById("multi-type");
+
+    multiButton.onclick = multiButtonClicked;
+
+    function multiButtonClicked() {
+        const type = multiTypeSelect.value;
+
+        const promises = [];
+
+        multiButton.disabled = true;
+        multiStatus.textContent = "Waiting for promises...";
+
+        for (let i = 0; i < 3; i++) {
+            promises.push(promiseFunction("{" + i + "}"));
+        }
+
+        const promise = (type === "all")
+            ? Promise.all(promises)
+            : Promise.race(promises);
+
+        promise.then(function (result) {
+            multiStatus.textContent = ("Success: " + ((type === "all") ? result.join(", ") : result));
+        }, function (result) {
+            multiStatus.textContent = ("Failure: " + result);
+        }).finally(function () {
+            multiButton.disabled = false;
+        });
+    }
+
+    function promiseFunction(identifier) {
         return new Promise(function (resolve, reject) {
 
-            window.setTimeout(function () {
-                resolve("Promise resolved.");
-            }, Math.random() * 5000);
+            const resolveMessage = identifier
+                ? "Promise " + identifier + " resolved."
+                : "Promise resolved.";
+
+            const rejectMessage = identifier
+                ? "Promise " + identifier + " rejected."
+                : "Promise rejected.";
 
             window.setTimeout(function () {
-                reject("Promise rejected.");
+                resolve(resolveMessage);
+            }, Math.random() * 2500);
+
+            window.setTimeout(function () {
+                reject(rejectMessage);
             }, Math.random() * 5000);
 
         });
